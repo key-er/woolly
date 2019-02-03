571c8432045091c72d58aa0c30710a32d054c73a
Nasa Quba <nasaquba@gmail.com>
2019-02-03 01:20:16 -0800

    moved run to deploy

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 echo "building.."
 docker build -t "$DOCKER_USERNAME"/woolly:woo .
-docker run -d -p 80:8080 --name  woo "$DOCKER_USERNAME"/woolly:woo
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker run -d -p 80:8080 --name  woo "$DOCKER_USERNAME"/woolly:woo
 docker push "$DOCKER_USERNAME"/woolly:woo
 echo "deploying....."
