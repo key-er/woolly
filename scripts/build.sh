e8e95d08fdbcaecc4a1960b93d20d455a6af1629
Nasa Quba <nasaquba@gmail.com>
2019-02-03 02:32:44 -0800

    list containers

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,5 +4,6 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build -t "$DOCKER_USERNAME"/woolly:woo .
 docker tag woo "$DOCKER_USERNAME"/woolly:woo
 docker images
-docker run -d -p 80:8080 --name  woo "$DOCKER_USERNAME"/woolly:woo
-docker exec -it woo /bin/sh -c "npm test"
+docker run -d -p 80:8080 --name woo "$DOCKER_USERNAME"/woolly:woo
+docker container ls
+# docker exec -it  /bin/sh -c "npm test"
