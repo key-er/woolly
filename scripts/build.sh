6118ccf3df259e74ce6b5e800ee941dd69c96a3a
Nasa Quba <nasaquba@gmail.com>
2019-02-03 02:17:27 -0800

    mv test to build

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,3 +4,5 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build -t "$DOCKER_USERNAME"/woolly:woo .
 docker tag woo "$DOCKER_USERNAME"/woolly:woo
 docker images
+docker run -d -p 80:8080 --name  woo "$DOCKER_USERNAME"/woolly:woo
+docker exec -it woo /bin/sh -c "npm test"
