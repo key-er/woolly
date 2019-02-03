0526b22c9feddfa3ee89f1cb45e427c1edc35803
Nasa Quba <nasaquba@gmail.com>
2019-02-03 02:00:46 -0800

    added missing tag

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 echo "building.."
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build -t "$DOCKER_USERNAME"/woolly:woo .
+docker tag woo "$DOCKER_USERNAME"/woolly:woo
+docker images
