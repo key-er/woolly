631d599105f2e00bc219f64953e9d89a1f39898a
Nasa Quba <nasaquba@gmail.com>
2019-02-03 01:31:48 -0800

    moved run to test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker run -d -p 80:8080 --name  woo "$DOCKER_USERNAME"/woolly:woo
 docker push "$DOCKER_USERNAME"/woolly:woo
 echo "deploying....."
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "testing..."
+docker run -d -p 80:8080 --name  woo "$DOCKER_USERNAME"/woolly:woo
 docker exec -it woo /bin/sh -c "npm test"
