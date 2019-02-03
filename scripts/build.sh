850fd1bb634f4edb4650a5232284091a70ec8e82
Nasa Quba <nasaquba@gmail.com>
2019-02-03 00:50:21 -0800

    added test

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 echo "building.."
+docker build -t cybercuffs/woolly:woo .
+docker run -d -p 80:8080 --name  woo cybercuffs/woolly:woo
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 echo "testing..."
+docker exec -it woo /bin/sh -c "npm test"
